f37085d8e7c9b80cb6b6957836cea29170a4ec97
Emily Lafontaine <emeraldkittia6@gmail.com>
2021-02-19 09:03:36 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_3a_Console_Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string UserName;
+            string Age;
+            string CurrentYear;
+            int resultAge;
+            int resultYear;
+            
+
+            Console.WriteLine("What is your name?");
+            UserName = Console.ReadLine();
+
+            Console.WriteLine("How old are you?");
+            Age = Console.ReadLine();
+            resultAge = Convert.ToInt32(Age);
+            
+            Console.WriteLine("What is the current year?");
+            CurrentYear = Console.ReadLine();
+            resultYear = Convert.ToInt32(CurrentYear);
+        }
+    }
+}
